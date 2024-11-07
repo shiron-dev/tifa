490cb99c4465db90a56603b57993d39a50f4f22f
Itsuki Shirahige <shiron4710.dev@gmail.com>
2024-11-07 19:34:02 +0900

    fix: env (#32)

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { Octokit } from "@octokit/core";
 
 import { markAllRenovateMergedNotificationsAsDone } from "./api";
 
+if (!process.env.GITHUB_TOKEN) {
+  import("dotenv/config");
+}
+
 async function main() {
   const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
   await markAllRenovateMergedNotificationsAsDone(octokit);
