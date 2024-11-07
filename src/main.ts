f221173ed1b935988bfc193699b4765745fc4c09
shiron <shiron4710.dev@gmail.com>
2024-11-07 19:36:06 +0900

    fix: env

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,7 @@ import { Octokit } from "@octokit/core";
 
 import { markAllRenovateMergedNotificationsAsDone } from "./api";
 
-if (!process.env.GITHUB_TOKEN) {
-  import("dotenv/config");
-}
+import "dotenv/config";
 
 async function main() {
   const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
