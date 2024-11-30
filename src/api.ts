04334e13dd02d07fe9f0732529d4aa3e6ee6ae2c
shiron <shiron4710.dev@gmail.com>
2024-11-30 15:28:34 +0900

    fix: error handling

--- src/api.ts
+++ src/api.ts
@@ -3,67 +3,62 @@ import type { Octokit } from "@octokit/rest";
 export async function markAllRenovateMergedNotificationsAsDone(
   octokit: Octokit,
 ) {
-  try {
-    let page = 1;
-    let hasMorePages = true;
+  let page = 1;
+  let hasMorePages = true;
 
-    while (hasMorePages) {
-      // eslint-disable-next-line no-console
-      console.log(`page: ${page}`);
-      const { data: notifications } = await octokit.rest.activity.listNotificationsForAuthenticatedUser(
+  while (hasMorePages) {
+    // eslint-disable-next-line no-console
+    console.log(`page: ${page}`);
+    const { data: notifications } = await octokit.rest.activity.listNotificationsForAuthenticatedUser(
+      {
+        all: false,
+        page,
+        per_page: 50,
+      },
+    );
+
+    if (notifications.length === 0) {
+      hasMorePages = false;
+      break;
+    }
+
+    for (const notification of notifications) {
+      const { subject, repository } = notification;
+      const threadId = Number(notification.id);
+
+      if (subject.type !== "PullRequest")
+        continue;
+
+      const prNumber = Number(subject.url.split("/").pop());
+      if (!repository.owner.login) {
+        console.error(`error owner: ${repository.owner.login}`);
+        continue;
+      }
+
+      const prResponse = await octokit.rest.pulls.get(
         {
-          all: false,
-          page,
-          per_page: 50,
+          owner: repository.owner.login,
+          repo: repository.name,
+          pull_number: prNumber,
         },
       );
+      const prData = prResponse.data;
+      const limit = prResponse.headers["x-ratelimit-remaining"];
 
-      if (notifications.length === 0) {
-        hasMorePages = false;
-        break;
+      if (prData.user.login === "renovate[bot]" && prData.merged) {
+        await octokit.rest.activity.markThreadAsDone({
+          thread_id: threadId,
+        });
+
+        // eslint-disable-next-line no-console
+        console.log(`done ${limit} ${repository.full_name} : ${prData.title}`);
       }
-
-      for (const notification of notifications) {
-        const { subject, repository } = notification;
-        const threadId = Number(notification.id);
-
-        if (subject.type !== "PullRequest")
-          continue;
-
-        const prNumber = Number(subject.url.split("/").pop());
-        if (!repository.owner.login) {
-          console.error(`error owner: ${repository.owner.login}`);
-          continue;
-        }
-
-        const prResponse = await octokit.rest.pulls.get(
-          {
-            owner: repository.owner.login,
-            repo: repository.name,
-            pull_number: prNumber,
-          },
-        );
-        const prData = prResponse.data;
-        const limit = prResponse.headers["x-ratelimit-remaining"];
-
-        if (prData.user.login === "renovate[bot]" && prData.merged) {
-          await octokit.rest.activity.markThreadAsDone({
-            thread_id: threadId,
-          });
-
-          // eslint-disable-next-line no-console
-          console.log(`done ${limit} ${repository.full_name} : ${prData.title}`);
-        }
-        else {
-          // eslint-disable-next-line no-console
-          console.log(`${limit} ${prData.user.login} : ${prData.merged}`);
-        }
+      else {
+        // eslint-disable-next-line no-console
+        console.log(`${limit} ${prData.user.login} : ${prData.merged}`);
       }
-
-      page += 1;
     }
-  }
-  catch (error) {
-    console.error("Error processing notifications:", error);
+
+    page += 1;
   }
 }
